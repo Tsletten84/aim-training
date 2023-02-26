425f65e0124d58e22ea427502895ef7554651a56
Tsletten84 <88718470+Tsletten84@users.noreply.github.com>
2023-02-26 14:54:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ target.addEventListener("click", function(){
     getRandom()
     time = 2
     points += 1
-    pointsEl.textContent = points
+    pointsEl.textContent =("Poeng" + points)
     target.style.marginTop = randomTop 
     target.style.marginLeft = randomWidth 
     // countdown.textContent = time
